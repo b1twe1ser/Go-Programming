16a6d1faa8a89b28156fb6ca39814752b2319fba
b1twe1ser <b1twe1ser@github.com>
2022-04-29 12:08:40 +0200

    Week 1 exercise solution

---
+++ Solutions/Week-1/go.mod
@@ -0,0 +1,3 @@
+module github.com/b1twe1ser/Week-1
+
+go 1.18
---
+++ Solutions/Week-1/main/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// task 2
+	fmt.Println("Programming is easy 😌")
+	fmt.Println("Programming is easy 😌")
+	fmt.Println("Programming is easy 😌")
+	fmt.Println("Programming is easy 😌")
+	fmt.Println("Programming is easy 😌")
+
+	// task 3
+	for i := 0; i < 9; i++ {
+		fmt.Println("This is so much fun 💖")
+	}
+}
